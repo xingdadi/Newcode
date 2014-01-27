4b6babf0a101b4a1d415adda95ac19b39774fa3f
xingdadi <dadixing@gmail.com>
2014-01-26 23:23:57 -0500

    new code
    for pm 2.5

---
+++ main_code.R
@@ -0,0 +1,11 @@
+#put the folder "myApp" in the wroking directory--getwd()
+library(shiny)
+
+library(MBA, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
+library(fields, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
+library(rgl, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
+library(graphics, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
+library(XML, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
+library(ggmap, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
+
+runApp("myApp")
