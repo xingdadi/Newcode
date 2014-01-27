fdc29bb0519758361845a5cd78fe61d5e2d98d7e
xingdadi <dadixing@gmail.com>
2014-01-27 01:07:33 -0500

    Update main_code.R

--- main_code.R
+++ main_code.R
@@ -5,17 +5,17 @@ x=1;
 print('x');
 
 
-library(MBA, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
+library(MBA, lib.loc='/home/ec2-user/Rlibrary')
 y=2;
 print('y');
 
-library(fields, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
+library(fields, lib.loc='/home/ec2-user/Rlibrary')
 z=3;
 print('z');
 
-library(rgl, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
-library(graphics, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
-library(XML, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
-library(ggmap, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
+library(rgl, lib.loc='/home/ec2-user/Rlibrary')
+library(graphics, lib.loc='/home/ec2-user/Rlibrary')
+library(XML, lib.loc='/home/ec2-user/Rlibrary')
+library(ggmap, lib.loc='/home/ec2-user/Rlibrary')
 
 runApp("myApp")
