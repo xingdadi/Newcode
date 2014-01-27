1e743d662368a7184395f0f64d3c6bdf1fcb6573
xingdadi <dadixing@gmail.com>
2014-01-26 23:40:30 -0500

    Update main_code.R

--- main_code.R
+++ main_code.R
@@ -1,8 +1,18 @@
 #put the folder "myApp" in the wroking directory--getwd()
 library(shiny)
 
+x=1;
+print('x');
+
+
 library(MBA, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
+y=2;
+print('y');
+
 library(fields, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
+z=3;
+print('z');
+
 library(rgl, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
 library(graphics, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
 library(XML, lib.loc='/home/ec2-user/R/x86_64-unknown-linux-gnu-library/2.15')
